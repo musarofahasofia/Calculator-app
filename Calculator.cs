51b558c76e9851ec21f151a138be8677ce94d580
sofi <sophiaaa@students.amikom.ac.id>
2023-03-17 14:49:57 +0700

    mengupdate file calculator

---
+++ Calculator.cs
@@ -0,0 +1,22 @@
+class Calculator
+{
+	static void main(sting[] args)
+	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil Penambahan: {0} + {1}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1}", a, b, Pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
+	}
+	static int penambahan (int a, int b)
+	{
+		return a + b;
+	}
+	static int pengurangan (int a, int b)
+	{
+		return a - b;
+	}
+}
